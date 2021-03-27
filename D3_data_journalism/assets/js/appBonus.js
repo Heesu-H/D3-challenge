37e18cc15e0d35e206e40c68ec78aa812b74b00f
Heesu-H <heesu456@hotmail.com>
2021-03-27 21:00:09 +1100

    completed base project

--- D3_data_journalism/assets/js/appBonus.js
+++ D3_data_journalism/assets/js/appBonus.js
@@ -9,8 +9,8 @@ var svgWidth = 960;
 //defining chart's margins as an object
 var margin = {
     top:30,
-    bottom:30,
-    left:30,
+    bottom:40,
+    left:45,
     right:30
 };
 
@@ -23,8 +23,19 @@ var svg = rowGraph.append('svg').attr('height', svgHeight).attr('width', svgWidt
 // appending a group to svg area and translating it to the right and down
 var chartGroup = svg.append('g').attr('transform',`translate(${margin.left},${margin.top})`)
 
+//creating scales FUNCTION
+// x scale
+// y scale
 
+//creating initial axis functions FUNCTION
+
+// appending x axis FUNCTION
+// appending y axis
 //reading in csv data
+
+//group for circles
+
+// group for state abbreviations
 d3.csv('assets/data/data.csv').then(data => {
 
     //parsing data  
@@ -36,12 +47,12 @@ d3.csv('assets/data/data.csv').then(data => {
 
     //creating scales
     // x scale
-    xScale = d3.scaleLinear().domain(d3.extent(percentPoverty)).range([0,chartWidth])
+    xScale = d3.scaleLinear().domain([d3.min(percentPoverty)-1,d3.max(percentPoverty)+1]).range([0,chartWidth])
     console.log(xScale)
     console.log(d3.extent(percentPoverty))
 
     // y scale
-    yScale = d3.scaleLinear().domain(d3.extent(percentLacksHealth)).range([chartHeight,0])
+    yScale = d3.scaleLinear().domain([d3.min(percentLacksHealth)-2,d3.max(percentLacksHealth)+2]).range([chartHeight,0])
     console.log(d3.extent(percentLacksHealth))
 
     //creating initial axis functions
@@ -79,7 +90,13 @@ d3.csv('assets/data/data.csv').then(data => {
         .attr('x', d => xScale(+d.poverty)-11)
         .attr('y', d => yScale(+d.healthcare)+5)
         .attr('fill','steelblue')
-       
-        
-        
+    
+    // labels group
+    var xLabelsGroup = chartGroup.append('g')
+        .attr("transform", `translate(${chartWidth/2},${chartHeight})`)
+
+    xLabelsGroup.append('text').attr('x',-30).attr('y',32).text("Percent Rate of Poverty")
+
+    var yLabelsGroup = chartGroup.append('g').attr('transform',`translate(0,${chartHeight/2}) rotate(-90)`);
+    yLabelsGroup.append('text').attr('x',-100).attr('y',-30).text('Percent Lack of Healthcare')
 })
